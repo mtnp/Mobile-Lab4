f62812d0332e71aaf6787d8c93d407596f34642e
mtnp <Silasp0801@gmail.com>
2022-10-25 14:36:23 -0500

    made main and associated activities

--- app/src/main/java/com/example/tripletriad/MainActivity.kt
+++ app/src/main/java/com/example/tripletriad/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.tripletriad
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.tripletriad.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Setup view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Launch the PlayActivity on playlBtn click
+        binding.playBtn.setOnClickListener { launchPlay() }
+
+        // Launch the AlbumActivity on albumBtn click
+        binding.albumBtn.setOnClickListener { launchAlbum() }
+
+        // Launch the SettingsActivity on settingsBtn click
+        binding.settingsBtn.setOnClickListener { launchSettings() }
+    }
+
+    private fun launchPlay() {
+        listIntent = Intent(this, PlayActivity::class.java)
+        startActivity(listIntent)
+    }
+
+    private fun launchAlbum() {
+        listIntent = Intent(this, AlbumActivity::class.java)
+        startActivity(listIntent)
+    }
+
+    private fun launchSettings() {
+        listIntent = Intent(this, SettingsActivity::class.java)
+        startActivity(listIntent)
     }
 }
