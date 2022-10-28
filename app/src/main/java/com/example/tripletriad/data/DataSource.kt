c5b7313a620914f0d5b28a11fc320d591a3fa413
mtnp <Silasp0801@gmail.com>
2022-10-27 23:53:29 -0500

    added some images and made cards

--- app/src/main/java/com/example/tripletriad/data/DataSource.kt
+++ app/src/main/java/com/example/tripletriad/data/DataSource.kt
@@ -12,33 +12,165 @@ object DataSource {
         ListCard (
             //dummy values
             R.drawable.blank_square,
-            "test1"
+            "Dodo",
+            4,
+            2,
+            3,
+            4
         ),
         ListCard (
             //dummy values
         R.drawable.blank_square,
-        "test2"
+        "Tonberry",
+            2,
+            2,
+            7,
+            2
         ),
 
         ListCard (
             R.drawable.blank_square,
-            "test3"
+            "Sabotender",
+            4,
+            3,
+            3,
+            3
         ),
         ListCard (
             R.drawable.blank_square,
-            "test4"
+            "Spriggan",
+            2,
+            3,
+            4,
+            4
         ),
         ListCard (
             R.drawable.blank_square,
-            "test5"
+            "Pudding",
+            2,
+            4,
+            3,
+            5
         ),
         ListCard (
             R.drawable.blank_square,
-            "test6"
+            "Bomb",
+            3,
+            4,
+            3,
+            3
         ),
         ListCard (
             R.drawable.blank_square,
-            "test7"
+            "Mandragora",
+            4,
+            2,
+            5,
+            3
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Coblyn",
+            3,
+            3,
+            3,
+            4
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Morbol",
+            5,
+            2,
+            2,
+            5
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Coeurl",
+            2,
+            5,
+            2,
+            5
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Ahriman",
+            5,
+            5,
+            2,
+            2
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Goobbue",
+            2,
+            5,
+            5,
+            2
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Chocobo",
+            3,
+            7,
+            2,
+            1
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Amalj\'aa",
+            1,
+            4,
+            7,
+            1
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Ixal",
+            6,
+            1,
+            3,
+            4
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Sylph",
+            2,
+            4,
+            5,
+            4
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Kobold",
+            2,
+            2,
+            4,
+            3
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Sahagin",
+            4,
+            5,
+            3,
+            3
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Tataru Taru",
+            7,
+            2,
+            3,
+            2
+        ),
+        ListCard (
+            R.drawable.blank_square,
+            "Moogle",
+            2,
+            1,
+            3,
+            7
         )
     )
 }
--- app/src/main/java/com/example/tripletriad/model/ListCard.kt
+++ app/src/main/java/com/example/tripletriad/model/ListCard.kt
@@ -4,5 +4,9 @@ import androidx.annotation.DrawableRes
 
 data class ListCard (
     @DrawableRes val imageResourceId: Int,
-    val name: String
+    val name: String,
+    val north: Int,
+    val east: Int,
+    val south: Int,
+    val west: Int
 )
