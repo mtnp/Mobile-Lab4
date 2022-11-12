040aca80af8ea792a8a5b0df08c262bf6cacae1a
mtnp <Silasp0801@gmail.com>
2022-11-12 14:02:08 -0600

    added back buttons and website launch

--- app/src/main/java/com/example/tripletriad/RulebookActivity.kt
+++ app/src/main/java/com/example/tripletriad/RulebookActivity.kt
@@ -2,10 +2,24 @@ package com.example.tripletriad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class RulebookActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rulebook)
+
+        webView = findViewById(R.id.webview);
+        webView.loadUrl("https://finalfantasy.fandom.com/wiki/Triple_Triad_(Final_Fantasy_VIII)")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 }
