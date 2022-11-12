18d3fc2e621524207b33ee823d61886ef88764c1
Nhat Tran <nhattran772@gmail.com>
2022-11-12 15:46:21 -0600

    WIP ROOM and added more randomness to AI

--- app/src/main/java/com/example/tripletriad/model/ListCard.kt
+++ app/src/main/java/com/example/tripletriad/model/ListCard.kt
@@ -1,9 +1,13 @@
 package com.example.tripletriad.model
 
 import androidx.annotation.DrawableRes
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.tripletriad.R
 
+@Entity(tableName = "card_table")
 data class ListCard (
+    @PrimaryKey(autoGenerate = true)
     @DrawableRes var imageResourceId: Int,
     val name: String,
     val north: Int,
