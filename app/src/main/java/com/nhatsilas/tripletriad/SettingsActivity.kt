e52594f2aec893fc3c0b2a6cde28cb405c6877b8
Nhat Tran <nhattran772@gmail.com>
2022-11-25 18:07:13 -0600

    minor changes

--- app/src/main/java/com/nhatsilas/tripletriad/SettingsActivity.kt
+++ app/src/main/java/com/nhatsilas/tripletriad/SettingsActivity.kt
@@ -1,17 +1,18 @@
 package com.nhatsilas.tripletriad
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.ImageView
 import android.widget.RadioButton
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.os.LocaleListCompat
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
+
 
 class SettingsActivity : AppCompatActivity() {
     var superLocale: Locale ?= null
+    var localeString: String ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -33,35 +34,49 @@ class SettingsActivity : AppCompatActivity() {
 //            MusicPlayer.playSound()
 //        }
 
+        val languageGroup: RadioGroup = findViewById(R.id.languageRadioGroup)
         val engBtn: RadioButton = findViewById(R.id.englishBtn)
         val spaBtn: RadioButton = findViewById(R.id.spanishBtn)
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val shEditor = sharedPreferences.edit()
+
+        superLocale = Locale(sharedPreferences.getString("language", "en"))
+        Locale.setDefault(superLocale)
+        resources.configuration.locale = superLocale
+        resources.configuration.setLayoutDirection(superLocale)
+        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+
+
 
         engBtn.setOnClickListener{
             val locale = Locale("en")
-            superLocale = Locale("en")
+            localeString = "en"
+            superLocale = Locale(localeString)
+            shEditor.putString("language", "es")
             Locale.setDefault(locale)
 
             resources.configuration.locale = locale
             resources.configuration.setLayoutDirection(locale)
             resources.updateConfiguration(resources.configuration, resources.displayMetrics)
-
         }
+
         spaBtn.setOnClickListener{
             val locale = Locale("es")
-            superLocale = Locale("es")
+            localeString = "es"
+            superLocale = Locale(localeString)
+            shEditor.putString("language", "es")
             Locale.setDefault(locale)
 
             resources.configuration.locale = locale
             resources.configuration.setLayoutDirection(locale)
             resources.updateConfiguration(resources.configuration, resources.displayMetrics)
-
         }
     }
 
     override fun onBackPressed() {
         startActivity(Intent(this, MainActivity::class.java))
-//        resources.configuration.locale = superLocale
-//        resources.configuration.setLayoutDirection(superLocale)
-//        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
+        resources.configuration.locale = superLocale
+        resources.configuration.setLayoutDirection(superLocale)
+        resources.updateConfiguration(resources.configuration, resources.displayMetrics)
     }
 }
