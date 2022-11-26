cecca8a1f4ce72ee719456544af3a0130f7cacd1
mtnp <Silasp0801@gmail.com>
2022-11-26 04:43:23 -0600

    made grid for album

--- app/src/main/java/com/nhatsilas/tripletriad/AlbumActivity.kt
+++ app/src/main/java/com/nhatsilas/tripletriad/AlbumActivity.kt
@@ -1,9 +1,12 @@
 package com.nhatsilas.tripletriad
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.nhatsilas.tripletriad.adapter.CardAdapter
 import com.nhatsilas.tripletriad.databinding.ActivityAlbumBinding
 
@@ -29,5 +32,10 @@ class AlbumActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
 
+        // if landscape mode, change span to 3
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            var recyclerView: RecyclerView = findViewById(R.id.album_recycler_view)
+            recyclerView.layoutManager = GridLayoutManager(this, 3)
+        }
     }
 }
--- app/src/main/java/com/nhatsilas/tripletriad/RulebookActivity.kt
+++ app/src/main/java/com/nhatsilas/tripletriad/RulebookActivity.kt
@@ -17,12 +17,16 @@ class RulebookActivity : AppCompatActivity() {
         webView = findViewById(R.id.webview)
         // enables the back button for google docs
         webView.setWebViewClient(WebViewClient())
+
         //wiki
 //        webView.loadUrl("https://finalfantasy.fandom.com/wiki/Triple_Triad_(Final_Fantasy_VIII)")
+
         // english rulebook
         webView.loadUrl("https://docs.google.com/document/d/1rCAHPoe0bDjFoa5NWxic3xiV9qJ2aimFN2oLq-j_Zrg/edit")
+
         // spanish rulebook
 //        webView.loadUrl("https://docs.google.com/document/d/1joMy9QJT27csr6W5uy527W_ECv242J3b/edit")
+
         //blog
 //        webView.loadUrl("https://medium.com/@silasp0801/cs371p-fall-2022-minh-triet-pham-47886f0ccac6")
 
